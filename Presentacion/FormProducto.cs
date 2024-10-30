ac96eecff6e9a3d4a6c0af5be45cc8775b179d05
Halosesparta <64061466+Halosesparta13@users.noreply.github.com>
2024-10-30 10:58:02 -0500

    Le agregué las clases y el form

--- Presentacion/FormProducto.cs
+++ Presentacion/FormProducto.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Presentacion
 {
-    public partial class Form1 : Form
+    public partial class FormProducto : Form
     {
-        public Form1()
+        public FormProducto()
         {
             InitializeComponent();
         }
